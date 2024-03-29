667ff5502273da0d3465f902a4dec8d2a388e249
Anthony Wong <ajw284@cornell.edu>
2024-03-29 11:51:20 -0400

    Added some Type multipliers

--- lib/MType.ml
+++ lib/MType.ml
@@ -1,4 +1,4 @@
-type mtype = 
+type mtype =
   | NO
   | Normal
   | Fire
@@ -18,3 +18,26 @@ type mtype =
   | Fighting
   | Electric
   | Ghost
+
+(**Note: call twice since pokemon have two types max*)
+let type_multiplier (t1 : mtype) (t2 : mtype) =
+  match (t1, t2) with
+  | _, NO -> 1.
+  | Normal, Ghost -> 0.
+  | Normal, Rock -> 0.5
+  | Normal, Steel -> 0.5
+  | Fire, Fire -> 0.5
+  | Fire, Water -> 0.5
+  | Fire, Grass -> 2.
+  | Fire, Ice -> 2.
+  | Fire, Bug -> 2.
+  | Fire, Rock -> 0.5
+  | Fire, Steel -> 2.
+  | Fire, Dragon -> 0.5
+  | Water, Fire -> 2.
+  | Water, Water -> 0.5
+  | Water, Grass -> 0.5
+  | Water, Ground -> 2.
+  | Water, Rock -> 2.
+  | Water, Dragon -> 0.5
+  | _, _ -> 1.
