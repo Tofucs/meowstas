009d400aa5c2ca7c994083c6986f611c5c8a8346
Raymond Lin <raymond.lin.05@gmail.com>
2024-05-12 21:35:40 -0400

    halfway done

--- lib/main_utils.ml
+++ lib/main_utils.ml
@@ -1,7 +1,7 @@
 open Tsdl
 open Tsdl_image
 
-let load_texture_from_file renderer file =
+let load_texture_from_png renderer file =
   match Image.load file with
   | Error (`Msg e) ->
       Sdl.log "Failed to load %s: %s" file e;
@@ -15,23 +15,57 @@ let load_texture_from_file renderer file =
           Sdl.free_surface surface;
           texture)
 
+let load_texture_from_bmp renderer file =
+  match Sdl.load_bmp file with
+  | Error (`Msg e) ->
+      Sdl.log "Failed to load %s: %s" file e;
+      exit 1
+  | Ok surface -> (
+      match Sdl.create_texture_from_surface renderer surface with
+      | Error (`Msg e) ->
+          Sdl.log "Create texture from surface error: %s" e;
+          exit 1
+      | Ok texture ->
+          Sdl.free_surface surface;
+          texture)
+
+type roaming_state = {
+  world : World.world;
+  mutable in_animation : bool;
+  mutable in_transition : bool;
+  mutable animation_start : int;
+  mutable animation_duration : int;
+  mutable start_pos : int * int;
+  mutable end_pos : int * int;
+}
+
+type battle_state = { mutable place_holder : string }
+
 type action_state =
   | Roaming
   | Battle
-  | Interlude
 
 type global_state = {
+  mutable is_running : bool;
   mutable renderer : Sdl.renderer;
   mutable window : Sdl.window;
   mutable window_size : int * int;
   mutable action_state : action_state;
+  mutable battle_state : battle_state option;
+  mutable roaming_state : roaming_state option;
   mutable world : World.world;
 }
 
-module type GameState = sig
-  type t = global_state option
+(* the global state is initialized in main, each action state is initialized by
+   the inits in each game mode *)
+module type GameMode = sig
+  type t = global_state
+
+  val init : t -> unit
+  (** initializes the state of that specific gamemode, only called the first
+      time the state is entered *)
 
   val update : t -> unit
   val render : t -> unit
-  val handle_event : t -> unit
+  val handle_events : t -> unit
 end
---
+++ lib/raymond_main.ml
@@ -0,0 +1,34 @@
+open Tsdl
+open Tile
+open Tsdl_image
+open Tsdl_ttf
+open Main_utils
+
+module BattleMode : GameMode = struct
+  type t = global_state
+
+  let extract_int opt =
+    match opt with
+    | Some i -> i
+    | None -> 0
+
+  let init state =
+    if state.battle_state = None then
+      state.battle_state <- Some { place_holder = "battle started!" }
+    else ()
+
+  let update state = print_int (fst state.window_size)
+
+  let render state =
+    let renderer = state.renderer in
+    let _ = Sdl.render_clear renderer in
+    Sdl.render_present renderer
+
+  let handle_events state =
+    let e = Sdl.Event.create () in
+    while Sdl.poll_event (Some e) do
+      match Sdl.Event.(enum (get e typ)) with
+      | `Quit -> state.is_running <- false
+      | _ -> ()
+    done
+end
